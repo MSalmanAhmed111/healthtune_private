f0b8ac42bdea81565315a8477f95c3a16e183dcd
Salman <salman.sa337@gmail.com>
2025-02-20 16:00:10 +0500

    fix(user): added user sync with webhook

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -2,3 +2,4 @@ export * from './base-entity';
 export * from 'src/sessions/entity';
 export * from 'src/macros/entity';
 export * from 'src/templates/entity';
+export * from 'src/user/entity'
---
+++ src/webhooks/clerk/clerk-webhook.service.ts
@@ -0,0 +1,49 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '@entities';
+import { SuccessResponseMessages } from '@messages';
+import { ApiMessageData } from '@types';
+
+@Injectable()
+export class ClerkWebhookService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async syncUser(reqBody): Promise<ApiMessageData> {
+    const { clerkUserId, email, firstName, lastName, username, imageUrl, publicMetadata, privateMetadata, unsafeMetadata } = reqBody;
+    console.log('reqBody: ', reqBody);
+    return { message: SuccessResponseMessages.successGeneral, data: reqBody };
+    let user = await this.userRepository.findOne({ where: [{ clerkUserId }, { email }] });
+
+    if (user) {
+      user.firstName = firstName ?? user.firstName;
+      user.lastName = lastName ?? user.lastName;
+      user.username = username ?? user.username;
+      user.imageUrl = imageUrl ?? user.imageUrl;
+      user.publicMetadata = publicMetadata ?? user.publicMetadata;
+      user.privateMetadata = privateMetadata ?? user.privateMetadata;
+      user.unsafeMetadata = unsafeMetadata ?? user.unsafeMetadata;
+
+      await this.userRepository.save(user);
+      return { message: SuccessResponseMessages.successGeneral, data: user };
+    } else {
+      user = this.userRepository.create({
+        clerkUserId,
+        email,
+        firstName,
+        lastName,
+        username,
+        imageUrl,
+        publicMetadata,
+        privateMetadata,
+        unsafeMetadata,
+      });
+
+      await this.userRepository.save(user);
+      return { message: SuccessResponseMessages.successGeneral, data: user };
+    }
+  }
+}
