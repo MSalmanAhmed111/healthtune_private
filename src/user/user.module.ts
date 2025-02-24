b1df612dd4ed8a17a7ae3eb3c620eaacee7e64ce
Salman <salman.sa337@gmail.com>
2025-02-24 17:58:24 +0500

    feat(macro): added delete api formacros

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { ClerkClientProvider } from 'src/common/providers';
+import { User } from '@entities';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService, ClerkClientProvider],
 })
