17b28f1d118c5d377e49167555d54fb80212d762
Salman <salman.sa337@gmail.com>
2025-03-28 14:58:14 +0500

    fix(setting): added types

--- src/setting/types/index.ts
+++ src/setting/types/index.ts
@@ -9,9 +9,11 @@ export const languageNames = languages.map((x) => x.name);
 export enum SettingNames {
   Language = 'Language',
   EnablePatientRecords = 'Enable patient records',
+  EnableAudioRecording = 'Enable audio recording',
 }
 
 export const SettingTypes: { [key in SettingNames]: string } = {
   [SettingNames.Language]: 'string',
   [SettingNames.EnablePatientRecords]: 'boolean',
+  [SettingNames.EnableAudioRecording]: 'boolean',
 };
